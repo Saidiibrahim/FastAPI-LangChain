9cbde879f4b231c8f4c8a382a0076bb66e3c255b
regirock365 <hemedi@protonmail.com>
2023-07-29 11:39:28 +0930

    streaming with chained messages

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
+import threading
+import queue
+
 import os
 from dotenv import load_dotenv
 from fastapi import FastAPI, Body
+from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from langchain.chat_models import ChatOpenAI
-from langchain.schema import (
-    HumanMessage,
-)
+from langchain.callbacks.base import BaseCallbackManager
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
-load_dotenv()
-OPENAI_API_KEY =os.getenv("OPENAI_API_KEY")
+from typing import List, Dict
 
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 app = FastAPI()
 
@@ -37,18 +41,84 @@ async def root():
 
 
 class ChatInput(BaseModel):
-    question: str
+    messages: List[Dict[str, str]]
+    model: str = ("gpt-3.5-turbo",)
+    temperature: float | None = (0.7,)
+    stream: bool | None = (False,)
+    user: str | None = (None,)
+    # description: str | None = None
+    # price: float
+    # tax: float | None = None
 
 
-@app.get("/chat/{question}")  # Adding question as a path parameter
-async def chat(question: str):
-    query = ChatOpenAI(
-        openai_api_key=OPENAI_API_KEY,
-        streaming=False,
-        callbacks=[StreamingStdOutCallbackHandler()],
-        temperature=0)
-    resp = query([HumanMessage(content=question)])
+class ThreadedGenerator:
+    def __init__(self):
+        self.queue = queue.Queue()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        item = self.queue.get()
+        if item is StopIteration:
+            raise item
+        return item
+
+    def send(self, data):
+        self.queue.put(data)
+
+    def close(self):
+        self.queue.put(StopIteration)
+
+
+class ChainStreamHandler(StreamingStdOutCallbackHandler):
+    def __init__(self, gen):
+        super().__init__()
+        self.gen = gen
+
+    def on_llm_new_token(self, token: str, **kwargs):
+        self.gen.send(token)
+
+
+def makeMessage(message: Dict[str, str]):
+    if message["role"] == "system":
+        return SystemMessage(content=message["content"])
+    if message["role"] == "assistant":
+        return AIMessage(content=message["content"])
+    if message["role"] == "user":
+        return HumanMessage(content=message["content"])
+    else:
+        return HumanMessage(content="unknown!")
+
+
+def llm_thread(g: ThreadedGenerator, body: ChatInput):
+    try:
+        query = ChatOpenAI(
+            openai_api_key=OPENAI_API_KEY,
+            verbose=True,
+            streaming=True,
+            callback_manager=BaseCallbackManager([ChainStreamHandler(g)]),
+            temperature=0,
+            # temperature=0.7,
+        )
+        body.messages
+        messageChain = list(map(makeMessage, body.messages))
+        query(messageChain)
+
+    finally:
+        g.close()
+
+
+def chat(body: ChatInput):
+    g = ThreadedGenerator()
+    threading.Thread(target=llm_thread, args=(g, body)).start()
+    return g
+
+
+@app.post("/chat/")  # Adding question as a path parameter
+async def stream(body: ChatInput):
+    return StreamingResponse(chat(body), media_type="text/event-stream")
     # Remove quotes and replace newline characters
-    content = resp.content.replace('"', '').replace('\n\n', '\n')
-    print(content)
-    return content
+    # content = resp.content.replace('"', "").replace("\n\n", "\n")
+    # print(content)
+    # return content
